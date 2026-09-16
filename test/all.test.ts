3c3b4b3326b1aec464ad56148aa8601e79f7b666
Anup Kumar Paul <83660548+Anuppaul@users.noreply.github.com>
2026-09-16 21:19:34 +0530

    test: include provider cache sanitizer and scanner suites

--- test/all.test.ts
+++ test/all.test.ts
@@ -2,3 +2,7 @@ import "./regression.test";
 import "./outputTokenLimit.test";
 import "./apiKeyValidation.test";
 import "./modelContextCatalog.test";
+import "./providerDefaults.test";
+import "./generationCacheIdentity.test";
+import "./outputSanitizer.test";
+import "./scannerPolicy.test";
